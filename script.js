78707fe1c0a93c189034ab72eb3a6acd3d9e64f4
magdy <mm246344@gmail.com>
2024-05-30 18:33:24 +0300

    second update in forth project

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 var indexNumber = document.getElementById("indexNumber");
 var websiteName = document.getElementById("websiteName");
+var addBtn = document.getElementById("addBtn");
+var updateBtn = document.getElementById("updateBtn");
+var thisIndex;
 var arrayList = [];
 if (localStorage.getItem("arrayList") != null) {
   arrayList = JSON.parse(localStorage.getItem("arrayList"));
@@ -23,15 +26,20 @@ function showData(array) {
     field += `<tr class="tr-table">
         <td class="p-2 text-center">${arrayList.indexOf(array[i])}</td>
         <td class="p-2 text-center">${array[i].name}</td>
-        <td class="p-2 text-center"><a class="text-decoration-none" href="https://${array[i].website}"><button onclick="visitItem()"
+        <td class="p-2 text-center"><a class="text-decoration-none" href="https://${
+          array[i].website
+        }"><button onclick="visitItem()"
         class="d-flex align-items-center mx-auto btn-visit btn text-white"><i
-            class="fa-solid fa-eye pe-2"></i>Visit</button></a></td>
-<td class="p-2 text-center"><button onclick="deleteItem()"
+            class="fa-solid fa-eye p-0 pe-sm-2"></i>Visit</button></a></td>
+<td class="p-2 text-center"><button onclick="deleteItem(${i})"
         class="d-flex align-items-center mx-auto btn-delete btn text-white"><i
-            class="fa-solid fa-trash-can pe-2"></i>Delete</button></td>
-            </tr>`
+            class="fa-solid fa-trash-can p-0 pe-sm-2"></i>Delete</button></td>
+<td class="p-2 text-center"><button onclick="getUpdate(${i})"
+        class="d-flex align-items-center mx-auto btn-update btn text-white"><i
+            class="fa-solid fa-cloud-upload-alt p-0 pe-sm-2"></i>Update</button></td>
+            </tr>`;
   document.getElementById("show-data").innerHTML = field;
-  storgeData()
+  storgeData();
 }
 
 function hideData(config) {
@@ -49,12 +57,27 @@ function deleteItem(i) {
   showData(arrayList);
 }
 
+function getUpdate(i) {
+  hideData(arrayList[i]);
+  thisIndex = i;
+  addBtn.classList.add("d-none");
+  updateBtn.classList.remove("d-none");
+}
+
+function updateItems() {
+  arrayList[thisIndex].name = indexNumber.value;
+  showData(arrayList);
+  storgeData();
+  updateBtn.classList.add("d-none");
+  addBtn.classList.remove("d-none");
+  hideData();
+}
+
 function searchByname(serachValue) {
-    var serachList = [];
-    for (i = 0; i < arrayList.length; i++)
-    if(arrayList[i].name.toLowerCase().includes(serachValue.toLowerCase()))
-        {
-            serachList.push(arrayList[i])
-        }
-        showData(serachList)
+  var serachList = [];
+  for (i = 0; i < arrayList.length; i++)
+    if (arrayList[i].name.toLowerCase().includes(serachValue.toLowerCase())) {
+      serachList.push(arrayList[i]);
     }
+  showData(serachList);
+}
